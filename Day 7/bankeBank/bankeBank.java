f04497b9c2d87f65ec092445aa552223d5f2db43
Hokage <oyebolatomilade@gmail.com>
2024-05-13 16:32:33 +0100

    More updates

--- Day 7/bankeBank/bankeBank.java
+++ Day 7/bankeBank/bankeBank.java
@@ -20,11 +20,21 @@ BANKE BANK
 
 """;
 
+String terms = """
+	TERMS AND CONDITIONS
+1. We will not be liable to any loss of funds
+2. We do not permit any crypto transactions
+3. Minimum deposit is a 10k
+4. There will be weekly charges of 5k for maintanace
+5. Transaction charges vary depending on mood (#500 - #1000);
+Do you agree to the following terms and conditions YES/NO.
+
+""";
 
 System.out.print(bank);
 int userSelection = input.nextInt();
 
-while(userSelection != 8 ){
+
 
 if (userSelection == 1){
 System.out.print("Please select your country for opening account: ");
@@ -47,10 +57,9 @@ char reuslt = email.charAt(email.length() - 1);
 System.out.print("What is your full name?  ");
 String name = input.next();
 
-System.out.print("What is your tel-phone number:");
-String number = input.next();
-System.out.print(" ");
- //number = input.next();
+System.out.print("What is your tel-phone number: ");
+String number = input.nextLine();
+input.nextLine();
 
 	if (number.length() > 11){
 	System.out.print("Invalid phone number");
@@ -59,8 +68,10 @@ System.out.print(" ");
 System.out.print("What is your address  ");
 String address = input.next();
 
-System.out.print("Please your bvn  ");
-String bvn = input.next();
+System.out.print("Please your bvn");
+String bvn = input.nextLine();
+System.out.print(" ");
+bvn = input.next();
 
 	if (bvn.length() > 11){
 	System.out.print("Invalid bvn number");		
@@ -78,13 +89,40 @@ String username = input.next();
 
 System.out.print("Choose a unique password ");
 String password = input.next();
+input.nextLine();
+System.out.print("Confirm password ");
+String confirmPassword = input.next();
 
+	while(!password.equals(confirmPassword)){
+		System.out.print("Password miss match ");
+		System.out.print("Choose a unique password ");
+		password = input.next();
+		System.out.print("Confirm password ");
+		confirmPassword = input.next();
+			} 
 
+System.out.print("\n"+terms);
+String answer = input.next();
+
+	if(answer.equalsIgnoreCase("yes")){
+	System.out.print("Account succesfully created\nPlease email your utility bill to the bankebank@gmail.com address\nOr drop it off at any branch");
+		}
+	else {System.out.print("Commot here abeg");}
 
 
 }
-else{ System.out.print("Our services are only available in Nigeria");}
-}
+
+else if (userSelection == 2) System.out.print("Are you sure you want to close this account?");
+
+//else{ System.out.print("Our services are only available in Nigeria");}
+
+
+
+
+
+
+
+
 
 
 
